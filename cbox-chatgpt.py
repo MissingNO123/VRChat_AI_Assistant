a175f5f1af4650a7c57faba1d22a98c0fc435742
Julia <MissingNO123@users.noreply.github.com>
2023-04-06 13:08:13 -0400

    Move some comments around and fix some things

--- cbox-chatgpt.py
+++ cbox-chatgpt.py
@@ -38,39 +38,38 @@ soundFeedback = True            # Play sound feedback when recording/stopped/mis
 audio_trigger_enabled = False   # Trigger voice recording on volume threshold
 key_trigger_key = Key.ctrl_r    # What key to double press to trigger recording
 key_press_window = 0.400        # How fast should you double click the key to trigger voice recording
-gpt = "GPT-4"           # GPT-3.5-Turbo-0301 | GPT-4
+gpt = "GPT-4"                   # GPT-3.5-Turbo-0301 | GPT-4
 max_tokens = 200                # Max tokens that openai will return
 in_dev_name = "VoiceMeeter Aux Output"  # Input  (mic)
 out_dev_name = "VoiceMeeter Aux Input"  # Output (tts)
 
 # elevenVoice = 'Bella'                # Voice to use with 11.ai
-elevenVoice = 'rMQzVEcycGrNzwMhDeq8' # The Missile Guidance System
+elevenVoice = 'rMQzVEcycGrNzwMhDeq8'   # The Missile Guidance System
 
-# adjust this to set the minimum volume threshold to start/stop recording
-THRESHOLD = 1024
-CHUNK_SIZE = 1024       # number of frames read at a time
-FORMAT = pyaudio.paInt16  # PCM format (int16)
-RATE = 16000  # 44100            # sample rate in Hz
-MAX_RECORDING_TIME = 30  # maximum recording time in seconds
-SILENCE_TIMEOUT = 2     # timeout in seconds for detecting silence
+
+THRESHOLD = 1024            # adjust this to set the minimum volume threshold to start/stop recording
+CHUNK_SIZE = 1024           # number of frames read at a time
+FORMAT = pyaudio.paInt16    # PCM format (int16)
+RATE = 16000  # 44100       # sample rate in Hz
+MAX_RECORDING_TIME = 30     # maximum recording time in seconds
+SILENCE_TIMEOUT = 2         # timeout in seconds for detecting silence
 OUTPUT_FILENAME = 'recording.wav'
 
 
 # System Prompts ##############################################################################################################################
-# VRChat System Prompt
+# VRChat AI Player System Prompt
 # systemPrompt = "You are an Artificial Intelligence in the VR social platform VRChat. Inside the game you take on the appearance of a player that other players can talk to. You can make conversation with people and answer their questions. If someone insults you, you are to dismiss them. Only reply in one sentence if possible."
 
 # Assistant System Prompt
-systemPrompt = f"You are an AI powered voice activated assistant. You are talking to a user. The user will talk directly to you, and you are able to reply back using text to speech. You can answer the user's questions to the best of your knowlege, but if you are unsure about something you have to tell them that you do not know enough about the subject. You have a silly and playful demeanor. You reply in only one sentence if possible."
+systemPrompt = "You are an AI powered voice activated assistant. You are talking to a user. The user will talk directly to you, and you are able to reply back using text to speech. You can answer the user's questions to the best of your knowlege, but if you are unsure about something you have to tell them that you do not know enough about the subject. You have a silly and playful demeanor. You reply in only one sentence if possible."
 
 # Insult system prompt
 # systemPrompt = "You are an AI powered voice activated insult machine named BratGPT. The user will talk directly to you, and you insult them no matter what they say. You only ever insult them, and do not say anything else. You talk like a catgirl with a hostile and condescending demeanor. You are using GPT-4, the latest AI language model from OpenAI. You reply in only one sentence if possible."
 
 
 # Variables ###################################################################################################################################
-model = None  # Whisper model object
-# List of messages sent back and forth between ChatGPT / User, initialized with example messages
-messageArr = []
+model = None    # Whisper model object
+messageArr = [] # List of messages sent back and forth between ChatGPT / User, initialized with example messages
 
 vb_out = None
 vb_in = None
@@ -89,7 +88,7 @@ speech_on = "Speech On.wav"
 speech_off = "Speech Sleep.wav"
 speech_mis = "Speech Misrecognition.wav"
 
-ip = "127.0.0.1"  # IP and Port for VRChat OSC
+ip = "127.0.0.1"  # IP and Ports for VRChat OSC
 inPort = 9000
 outPort = 9001
 
@@ -186,7 +185,7 @@ def old_whisper_transcribe():
     if result.no_speech_prob > 0.5:
         vrc_chatbox('⚠ [unintelligible]')
         if (soundFeedback): play_sound_threaded(speech_mis)
-        print(f"U: {result.no_speech_prob*100:.1f}%")
+        verbose_print(f"U: {result.no_speech_prob*100:.1f}%")
         tts('I didn\'t understand that!', 'en')
         vrc_set_parameter('VoiceRec_End', True)
         vrc_set_parameter('CGPT_Result', True)
@@ -478,7 +477,7 @@ def handle_command(command):
     global audio_trigger_enabled
     match command:
         case 'reset':
-            messageArr = []  # List of messages sent back and forth between ChatGPT / User
+            messageArr = []
             print(f'$ Messages cleared!')
             vrc_chatbox('🗑️ Cleared message buffer')
             play_sound('./prebaked_tts/Clearedmessagebuffer.wav')
@@ -601,8 +600,8 @@ def load_whisper():
     vrc_chatbox('🔄 Loading Voice Recognition...')
     start_time = time.time()
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # model = whisper.load_model(whisper_model, device, in_memory=True)
-    model = WhisperModel(whisper_model, device='cuda', compute_type="int8")
+    # model = whisper.load_model(whisper_model, device, in_memory=True) # OpenAI Whisper
+    model = WhisperModel(whisper_model, device='cuda', compute_type="int8") # FasterWhisper
     end_time = time.time()
     verbose_print(f'--Whisper loaded in {end_time - start_time:.3f}s')
     vrc_chatbox('✔️ Voice Recognition Loaded')
