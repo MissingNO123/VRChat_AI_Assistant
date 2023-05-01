1cdf2bd1035cbdbfe2c6bfca180205fe63562044
Julia <MissingNO123@users.noreply.github.com>
2023-04-30 20:07:42 -0400

    Add EZ start scripts

---
+++ start_assistant.bat
@@ -0,0 +1,7 @@
+@echo off
+
+rem Activate the Virtual Environment
+call .\venv\Scripts\activate.bat
+
+rem Run the script
+python assistant.py
---
+++ start_assistant.ps1
@@ -0,0 +1,5 @@
+# Activate the Virtual Environment
+& .\venv\Scripts\Activate.ps1
+
+# Run the script
+python assistant.py
