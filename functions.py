b2ac0d18ad7aeab38deba57c3a6eb8e4a473613c
MissingNO123 <MissingNO123@users.noreply.github.com>
2023-11-30 10:47:27 -0500

    Implement LogWatcher, also fix text cutter

--- functions.py
+++ functions.py
@@ -5,6 +5,9 @@ import time
 import struct
 import threading
 from io import BytesIO
+import requests
+import os
+import psutil
 
 import vrcutils as vrc
 import options as opts
@@ -13,6 +16,9 @@ import texttospeech as ttsutils
 vb_out = None
 vb_in = None
 pyAudio = None
+
+vrc_request_timeout = time.time()
+vrc_player_count = ""
 
 empty_audio = BytesIO(b"\x52\x49\x46\x46\x52\x49\x00\x00\x57\x41\x56\x45\x66\x6d\x74\x20\x10\x00\x00\x00\x01\x00\x01\x00\x44\xac\x00\x00\x88\x58\x01\x00\x02\x00\x10\x00\x64\x61\x74\x61\x00\x00\x00\x00")
 
@@ -168,15 +174,29 @@ def clip_audio_end(audio_bytes: BytesIO) -> BytesIO:
 
 def cut_up_text(text):
     """ Cuts text into segments of 144 chars that are pushed one by one to VRC Chatbox """
-    # Check if text has whitespace or punctuation
-    if re.search(r'[\s.,?!]', text):
-        # Split the text into segments of up to 141 characters using the regex pattern
-        # 141 since emoji (2 bytes) + a space (1 byte) 
-        segments = re.findall(
-            r'.{1,141}(?<=\S)(?=[,.?!]?\s|$)|\b.{1,141}\b', text)
-    else:
-        # Split the text into chunks of up to 144 characters using list comprehension
-        segments = [text[i:i+141] for i in range(0, len(text), 141)]
+    # # Check if text has whitespace or punctuation
+    # if re.search(r'[\s.,?!]', text):
+    #     # Split the text into segments of up to 141 characters using the regex pattern
+    #     # 141 since emoji (2 bytes) + a space (1 byte) 
+    #     segments = re.findall(
+    #         r'.{1,141}(?<=\S)(?=[,.?!]?\s|$)|\b.{1,141}\b', text)
+    # else:
+    #     # Split the text into chunks of up to 144 characters using list comprehension
+    #     segments = [text[i:i+141] for i in range(0, len(text), 141)]
+
+    txt = text
+    segments = []
+    while len(txt) > 144:
+        substrings = [' ', '.', ',', '!', '?', ')', ']', '}', '>', ':', ';', '\'', '"', '\n']
+        last_indices = [txt[:144].rfind(sub) for sub in substrings]
+        last_space_index = max(last_indices)
+        if last_space_index == -1:
+            last_space_index = 144
+        chunk = txt[:last_space_index]
+        segments.append(chunk)
+        txt = txt[last_space_index+1:]
+    segments.append(txt)
+
     i = 0
     list = []
     for i, segment in enumerate(segments):
@@ -210,9 +230,209 @@ def tts(text):
     audioBytes.close()
     opts.speaking = False
 
+    
+class LogWatcher:
+    def __init__(self):
+        self.running = False
+        self.vrc_is_running = False
+        self.last_line = 0
+        self.log_file = None
+        self.log_directory = None
+        self.player_count = 0
+        self.player_list = []
+        self.world_name = ""
+        self.full_world_id = ""
+        self.world_id = ""
+        self.instance_id = ""
+        self.instance_privacy = ""
+        self.process_monitor_thread = threading.Thread(target=self._monitor_vrchat)
+        self.log_watcher_thread = threading.Thread(target=self._watch_log_file)
+        self.log_directory = os.path.join(os.path.expanduser("~"), 'AppData', 'LocalLow', 'VRChat', 'VRChat')
 
-def read_player_list():
-    raise NotImplementedError
+    def start(self):
+        self.running = True
+        self.process_monitor_thread = threading.Thread(target=self._monitor_vrchat)
+        self.process_monitor_thread.start()
 
-def read_player_count():
-    raise NotImplementedError
+    def stop(self):
+        self.running = False
+        self.process_monitor_thread.join()
+
+    def _check_vrchat_running(self):
+        for proc in psutil.process_iter(['pid', 'name']):
+            if proc.info['name'].lower() == 'vrchat.exe':
+                return True
+        return False
+
+    def _monitor_vrchat(self):
+        while opts.LOOP and self.running:
+            self.vrc_is_running = self._check_vrchat_running()
+            if self.vrc_is_running:
+                    if self.log_file is None: 
+                        self.log_file = self._get_log_file(self.log_directory)
+                    if not self.log_watcher_thread.is_alive():
+                        self.log_watcher_thread = threading.Thread(target=self._watch_log_file)
+                        self.log_watcher_thread.start()
+            else:
+                if self.log_watcher_thread.is_alive():
+                    self.log_watcher_thread.join()
+                    self.log_file = None
+            time.sleep(30)
+
+    def _watch_log_file(self):
+        while self.vrc_is_running:
+            time.sleep(30)
+            if self.log_file:
+                new_last_line = len(open(self.log_file, encoding='utf-8').readlines())
+                if self.last_line < new_last_line:
+                    with open(self.log_file, "r", encoding='utf-8') as file:
+                        lines = file.readlines()[self.last_line:new_last_line]
+                        if lines:
+                            self._parse_log_lines(lines)
+                    self.last_line = new_last_line
+
+    def _get_log_file(self, directory):
+        all_files = [os.path.join(directory, file) for file in os.listdir(directory)]
+        files = [file for file in all_files if os.path.isfile(file)]
+        if not files:
+            return None
+        most_recent_file = max(files, key=os.path.getmtime)
+        return most_recent_file
+
+    def _parse_log_lines(self, lines):
+        for line in lines:
+            if line != "" and line != "\n":
+                l = line.strip()
+                self._parse_log_location(l)
+                self._parse_log_on_player_joined_or_left(l)
+
+    def _parse_log_location(self, line):
+        if '[Behaviour] Entering Room: ' in line or '[RoomManager] Entering Room:' in line:
+            self.player_count = 0
+            self.player_list = []
+            line_offset = line.rfind("] Entering Room: ")
+            if (line_offset < 0):
+                return
+            substring_start = line_offset + 17
+            if substring_start >= len(line):
+                return
+            self.world_name = line[substring_start:]
+        if '[Behaviour] Joining wrld_' in line:
+            line_offset = line.rfind("[Behaviour] Joining ")
+            if (line_offset < 0):
+                return
+            substring_start = line_offset + 20
+            if substring_start >= len(line):
+                return
+            self.full_world_id = line[substring_start:]
+
+            # Regular expression pattern to match world:id
+            pattern = r'wrld_([\w-]+):(\d+)'
+            match_world = re.match(pattern, self.full_world_id)
+            if not match_world:
+                return
+
+            self.world_id, self.instance_id = match_world.groups()
+
+            # Regular expression pattern to match ~tag(id)
+            pattern = r'~(\w+)(?:\(([\w-]+)\))?'
+            tags = {}
+            match_tag = re.finditer(pattern, self.full_world_id)
+            for matches in match_tag:
+                tag = matches.group(1)
+                tag_id = matches.group(2)
+                tags[tag] = tag_id if tag_id else None
+
+            instance_privacy_map = {
+                'public': 'Public',
+                'hidden': 'Friends+',
+                'friends': 'Friends',
+                'private': 'Invite',
+                'group': 'Group'
+            }
+
+            group_privacy_map = {
+                'group': 'Group Members Only', 
+                'plus': 'Group Plus', 
+                'groupPlus': 'Group Plus', 
+                'public': 'Group Public', 
+            }
+
+            for tag, value in tags.items():
+                if tag in ['public', 'hidden', 'friends', 'private']:
+                    self.instance_privacy = instance_privacy_map[tag]
+                if tag == 'group':
+                    self.instance_privacy = 'Group'
+                if tag == 'groupAccessType':
+                    self.instance_privacy = group_privacy_map[value]
+                if opts.verbosity:
+                    if value is not None:
+                        print(f"Key: {tag},    Value: {value}")
+                    else:
+                        print(f"Key: {tag}")
+
+    def _parse_log_on_player_joined_or_left(self, line):
+        if  (
+            ('[Behaviour] OnPlayerJoined' in line or '[NetworkManager] OnPlayerJoined' in line) 
+             and not '] OnPlayerJoined:' in line
+        ):
+            self.player_count += 1
+            line_offset = line.rfind('] OnPlayerJoined')
+            if line_offset < 0:
+                return
+            substring_start = line_offset + 17
+            if substring_start >= len(line):
+                return
+            user_display_name = line[substring_start:]
+            self.player_list.append(user_display_name)
+        elif (
+            ('[Behaviour] OnPlayerLeft' in line or '[NetworkManager] OnPlayerLeft' in line) 
+            and not 
+            ('] OnPlayerLeftRoom' in line or '] OnPlayerLeft:' in line)
+        ):
+            self.player_count -= 1
+            line_offset = line.rfind('] OnPlayerLeft')
+            if line_offset < 0:
+                return
+            substring_start = line_offset + 15
+            if substring_start >= len(line):
+                return
+            user_display_name = line[substring_start:]
+            if user_display_name in self.player_list:
+                self.player_list.remove(user_display_name)
+
+log_parser = LogWatcher()
+log_parser.start()
+
+
+def get_player_list():
+    if log_parser is None: 
+        return None
+    if log_parser.running is False: 
+        return []
+    return log_parser.player_list
+
+
+def get_player_count():
+    if log_parser is None: 
+        return None
+    if log_parser.running is False:
+        return 0
+    return log_parser.player_count
+
+
+def get_vrchat_player_count():
+    global vrc_request_timeout
+    global vrc_player_count
+    if time.time() < vrc_request_timeout:
+        return vrc_player_count
+    headers = {
+        'User-Agent': 'VRChatAIAssistant/0.0.1 github.com/MissingNO123',
+    }
+    response = requests.get('https://api.vrchat.cloud/api/1/visits/', headers=headers)
+    if response.status_code == 200:
+        vrc_player_count = response.text
+        vrc_request_timeout = time.time() + 60
+        return response.text
+    else:
+        return None
